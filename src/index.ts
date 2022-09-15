4e97e8d67756a93e1e25171e587824dcf4730519
Elijah Green <elijahgreen@gmail.com>
2022-09-14 17:54:22 -0700

    Add get video by Id

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { VideoObject, WithContext } from "schema-dts";
 import "videogata-plugin-typings";
 import { MessageType, UiMessageType } from "./types";
+import { parse, toSeconds } from "iso8601-duration";
 
 const rumbleUrl = "https://rumble.com";
 
@@ -21,6 +23,11 @@ function sendMessage(message: MessageType) {
 }
 
 async function getVideoId(apiId: string): Promise<string> {
+  const video = await getVideoById(apiId);
+  return video.apiId || "";
+}
+
+const getVideoById = async (apiId: string) => {
   const url = `${rumbleUrl}/${apiId}`;
   let proxy = await application.getCorsProxy();
   if (!proxy) {
@@ -34,14 +41,62 @@ async function getVideoId(apiId: string): Promise<string> {
 
   const jsonNode = html.querySelector('script[type="application/ld+json"]');
   const jsonLd: any[] = JSON.parse(jsonNode?.textContent || "");
-  const videoObject = jsonLd.filter((j) => j["@type"] === "VideoObject")[0];
+  const videoObject: WithContext<VideoObject> = jsonLd.filter(
+    (j) => j["@type"] === "VideoObject"
+  )[0];
 
   // https://rumble.com/embed/{videoId}/
-  const embedUrl: string = videoObject.embedUrl;
+  const embedUrl: string = videoObject.embedUrl?.toString() || "";
   const videoId = embedUrl.split("/").slice(0, -1).slice(-1)[0];
+  const uploadDate = videoObject.uploadDate?.toString();
+  const name = videoObject.name?.toString();
+  const durationStr = videoObject.duration?.toString();
+  let duration = 0;
+  if (durationStr) {
+    duration = toSeconds(parse(durationStr));
+  }
+  const originalUrl = videoObject.url?.toString();
+  const thumbnailUrl = videoObject.thumbnailUrl?.toString();
+  let views: number | undefined;
+  if (
+    videoObject.interactionStatistic &&
+    "userInteractionCount" in videoObject.interactionStatistic
+  ) {
+    views =
+      videoObject.interactionStatistic.userInteractionCount?.valueOf() as any;
+  }
 
-  return videoId;
-}
+  const channelName =
+    html.getElementsByClassName("media-heading-name")[0].textContent || "";
+
+  const link = html.getElementsByClassName(
+    "media-by--a"
+  )[0] as HTMLAnchorElement;
+  // const channelApiId = link.getAttribute("href")?.split("/").slice(-1)[0];
+
+  const video: Video = {
+    title: name || "",
+    channelName: channelName,
+    // channelApiId,
+    duration,
+    apiId: videoId,
+    uploadDate,
+    images: thumbnailUrl
+      ? [
+          {
+            url: thumbnailUrl,
+          },
+        ]
+      : undefined,
+    originalUrl,
+    views,
+  };
+  return video;
+};
+
+const getVideo = async (request: GetVideoRequest) => {
+  return await getVideoById(request.apiId);
+};
 
 application.onUiMessage = async (message: UiMessageType) => {
   switch (message.type) {
@@ -58,26 +113,26 @@ application.onUiMessage = async (message: UiMessageType) => {
 const videoListingToVideo = (listing: Element): Video => {
   // title
   const title =
-    listing.getElementsByClassName("video-item--title")[0].textContent || "";
+    listing.getElementsByClassName("video-item--title")[0]?.textContent || "";
 
   //duration
   const durationElement = listing.getElementsByClassName(
     "video-item--duration"
   )[0];
-  const timeStr = durationElement.getAttribute("data-value");
+  const timeStr = durationElement?.getAttribute("data-value");
   const duration = hmsToSecondsOnly(timeStr || "0");
 
   //images
   const imgElement = listing.getElementsByClassName(
     "video-item--img"
   )[0] as HTMLImageElement;
-  const imageSrc = imgElement.src;
+  const imageSrc = imgElement?.src;
 
   // apiId
   const link = listing.getElementsByClassName(
     "video-item--a"
   )[0] as HTMLAnchorElement;
-  const apiId = link.getAttribute("href")?.substring(1).split("0")[0];
+  const apiId = link?.getAttribute("href")?.substring(1).split("0")[0];
 
   return {
     title,
@@ -118,6 +173,7 @@ const searchAll = async (request: SearchRequest): Promise<SearchAllResult> => {
 
 application.onSearchAll = searchAll;
 application.onSearchVideos = searchVideos;
+application.onGetVideo = getVideo;
 
 const init = () => {};
 
